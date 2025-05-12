52651fd74b4a08b7a32b36d3f368098aa1db0049
Conner Porter <porterc@students.westerntc.edu>
2025-05-12 12:38:31 -0500

    May 12th Class

---
+++ Class-Work/SchoolSystem/Migrations/20250512172106_FixedSpellingMigration.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedSpellingMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Instructiors_InstructorId",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Instructiors_Departments_DepartmentId",
+                table: "Instructiors");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Instructiors",
+                table: "Instructiors");
+
+            migrationBuilder.RenameTable(
+                name: "Instructiors",
+                newName: "Instructors");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Instructiors_DepartmentId",
+                table: "Instructors",
+                newName: "IX_Instructors_DepartmentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Instructors",
+                table: "Instructors",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Instructors_InstructorId",
+                table: "Courses",
+                column: "InstructorId",
+                principalTable: "Instructors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Instructors_Departments_DepartmentId",
+                table: "Instructors",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Instructors_InstructorId",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Instructors_Departments_DepartmentId",
+                table: "Instructors");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Instructors",
+                table: "Instructors");
+
+            migrationBuilder.RenameTable(
+                name: "Instructors",
+                newName: "Instructiors");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Instructors_DepartmentId",
+                table: "Instructiors",
+                newName: "IX_Instructiors_DepartmentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Instructiors",
+                table: "Instructiors",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Instructiors_InstructorId",
+                table: "Courses",
+                column: "InstructorId",
+                principalTable: "Instructiors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Instructiors_Departments_DepartmentId",
+                table: "Instructiors",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
